9ead4c00137bdae90af7670e94c3f7d1b6e08a1b
My Secont Github Repo <hmsadikurrahman120@gmail.com>
2023-04-06 21:53:46 +0600

    ad to nasted router

--- src/main.jsx
+++ src/main.jsx
@@ -12,17 +12,34 @@ import Contact from './component/Contact/Contact';
 import Header from './component/Header/Header';
 import ErrorPage from './component/ErrorPage/ErrorPage';
 
+// const router = createBrowserRouter([
+//   {
+//     path: '/',
+//     element: <About></About>,
+//     errorElement: <ErrorPage></ErrorPage>
+//   }
+// ])
+
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <About></About>,
-    errorElement: <ErrorPage></ErrorPage>
+    element: <Home></Home>,
+    errorElement: <ErrorPage></ErrorPage>,
+    children: [
+      {
+        path: '/about',
+        element: <About></About>
+      },
+      {
+        path: '/contact',
+        element: <Contact></Contact>
+      }
+    ]
   }
 ])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Header></Header>
     <RouterProvider router={router}></RouterProvider>
   </React.StrictMode>,
 )
