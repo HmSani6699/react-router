d4617a52be1d5476f0d03991c4e676771dde987b
My Secont Github Repo <hmsadikurrahman120@gmail.com>
2023-04-06 19:42:26 +0600

    ad to react router

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,18 @@ import {
 } from "react-router-dom";
 
 const router = createBrowserRouter([
-
+  {
+    path: '/',
+    element: <App></App>
+  },
+  {
+    path: '/home',
+    element: <div>Hallo home container vsi</div>
+  },
+  {
+    path: 'about',
+    element: <div><h1>This is About component</h1></div>
+  }
 ])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
